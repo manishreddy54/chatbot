646303319e870622c99fcea16908a47e1897bb0e
manish <manishreddy30@icloud.com>
2025-05-26 17:49:59 -0500

    Describe your change here

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ retriever = vectorstore.as_retriever()
 llm = ChatGroq(
     model_name = "llama3-70b-8192",
     api_key=os.environ.get("GROQ_API_KEY")
-
+)
 
 def get_memory(session_id):
     return InMemoryChatMessageHistory()
